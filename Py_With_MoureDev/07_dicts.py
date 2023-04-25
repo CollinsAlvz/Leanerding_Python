da2691803de8f69e50a5877298a85d843d808a8f
Collins <k.collinsalvarezherrera@gmail.com>
2023-04-25 16:49:40 -0600

    terminacion de los dicts

--- Py_With_MoureDev/07_dicts.py
+++ Py_With_MoureDev/07_dicts.py
@@ -1,4 +1,6 @@
-### DIctionaries ###
+### Dictionaries ###
+
+# tipo de almacenamiento clave/valor
 
 my_dict = dict()
 
@@ -8,5 +10,25 @@ print(type(my_dict))
 print(type(my_other_dict))
 
 my_other_dict = {"Nombre": "Collins", "Apellido": "Alvarez", "Edad": 24}
-
 print(my_other_dict)
+
+my_dict = {"Nombre": "Collins", "Apellido": "Alvarez", "Edad": 24,
+           "Lenguajes": {"C#", "JavaScript", "Python"}
+           }
+print(my_dict)
+
+# Se agrega un nuevo campo
+my_dict["BD"] = "SQL"
+print(my_dict)
+
+# Eliminacio de un campo
+del my_dict["BD"]
+print(my_dict)
+
+
+print("Nombre" in my_dict)
+
+# Operaciones
+print(my_dict.items())
+print(my_dict.keys())
+print(my_dict.values())
