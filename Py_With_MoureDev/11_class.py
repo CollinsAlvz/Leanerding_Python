1a5516b396daced11be64dd421b7a32133dff1df
Collins <k.collinsalvarezherrera@gmail.com>
2023-04-29 14:49:39 -0600

    commit finalizacion del 11_class

--- Py_With_MoureDev/11_class.py
+++ Py_With_MoureDev/11_class.py
@@ -7,14 +7,22 @@ class MyEmptyPerson:
 class Person:
     def __init__(self, name, surname) -> None:
         self.name = name
+        self.__name = name  # se privatiza con dos guiones bajos + el nombre que se le de
         self.surname = surname
         self.full_name = f"{name} {surname}"
 
     def walk(self):
         print(f"{self.full_name} está caminando")
 
+    def get_name(self):
+        return self.__name
+
+    def set_name(self, name):
+        self.__name = name
+
 
 my_person = Person("Collins", "Alvarez")
 print(my_person.name)
 print(my_person.full_name)
 my_person.walk()
+print(my_person.get_name())
